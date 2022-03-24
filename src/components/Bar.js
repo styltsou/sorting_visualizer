f6f45c36feb6577f40375a7ea5eb552d521ddb11
styltsou <tsoumanis.stylianos@gmail.com>
2022-03-24 19:07:41 +0200

    Refactoring to use functional components and removed some buttons

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Div = styled.div`
-  width: .6rem;
-  height: ${props => `${props.height / 10}rem`};
-  margin: auto 1px 0 1px;
-  background-color: ${props =>
-    props.isCompared === true
-      ? 'var(--color-green)'
-      : 'var(--color-primary)'};
+	width: 0.6rem;
+	height: ${(props) => `${props.height / 10}rem`};
+	margin: auto 1px 0 1px;
+	background-color: ${(props) =>
+		props.isCompared === true ? "var(--color-green)" : "var(--color-primary)"};
 `;
 
 const Bar = ({ height, isCompared }) => {
-  return <Div height={height} isCompared={isCompared}/>;
+	return <Div height={height} isCompared={isCompared} />;
 };
 
 export default Bar;
--- src/containers/SortingVisualizer.js
+++ src/containers/SortingVisualizer.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
-import resetArray from '../utils/resetArray';
-import bubbleSort from '../utils/sorting/bubbleSort';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import resetArray from "../utils/resetArray";
+import bubbleSort from "../utils/sorting/bubbleSort";
 // import swap from '../utils/swap';
 
-import Bar from '../components/Bar';
-import Button from '../components/Button';
+import Bar from "../components/Bar";
+import Button from "../components/Button";
 
 const FLexContainer = styled.div`
-  display: flex;
-  max-width: 100%;
-  flex-direction: row;
-  background-color: var(--color-white);
-  box-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.1);
+	display: flex;
+	max-width: 100%;
+	flex-direction: row;
+	background-color: var(--color-white);
+	box-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.1);
 `;
 
 const ToolbarContainer = styled.div`
-  display: inline-flex;
-  flex-direction: row;
+	display: inline-flex;
+	flex-direction: row;
 `;
 
 const SortButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-  flex-grow: 1;
+	display: flex;
+	flex-direction: row-reverse;
+	flex-grow: 1;
 `;
 
 const MainContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  position: absolute;
-  width: 100vw;
-  height: 90vh;
-  bottom: 0px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	position: absolute;
+	width: 100vw;
+	height: 90vh;
+	bottom: 0px;
 `;
 
 // VISUALIZATION OPTIONS
@@ -41,86 +41,89 @@ const ARRAY_SIZE = 80;
 const UPDATE_SPEED = 5;
 const RESET_TIMEOUT = 1500;
 
-class SortingVisualizer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { array: [], compared: [] };
-  }
+const SortingVisualizer = () => {
+	const [state, setState] = useState({ array: [], compared: [] });
 
-  componentDidMount() {
-    this.setState({ array: resetArray(ARRAY_SIZE) });
-    // this.sortArray();
-  }
+	const generateArray = () => {
+		setState((prev) => ({
+			...prev,
+			array: resetArray(ARRAY_SIZE),
+		}));
+	};
 
-  generateArray = () => {
-    this.setState({ array: resetArray(ARRAY_SIZE) });
-  };
+	const sortArray = () => {
+		const arr = [...state.array];
+		const { comparisons, swaps } = bubbleSort(arr);
+		let newArr = [...state.array];
+		let comparedElements = [];
+		let i = 0;
 
-  sortArray = () => {
-    const arr = [...this.state.array];
-    const { comparisons, swaps } = bubbleSort(arr);
-    let newArr = [...this.state.array];
-    let comparedElements = [];
-    let i = 0;
+		const intervalID = setInterval(() => {
+			if (i > 0) {
+				comparedElements[comparisons[i - 1].idxa] = false;
+				comparedElements[comparisons[i - 1].idxb] = false;
+			}
+			comparedElements[comparisons[i].idxa] = true;
+			comparedElements[comparisons[i].idxb] = true;
 
-    const intervalID = setInterval(() => {
-      if (i > 0) {
-        comparedElements[comparisons[i - 1].idxa] = false;
-        comparedElements[comparisons[i - 1].idxb] = false;
-      }
-      comparedElements[comparisons[i].idxa] = true;
-      comparedElements[comparisons[i].idxb] = true;
+			let temp = newArr[swaps[i].idxa];
+			newArr[swaps[i].idxa] = newArr[swaps[i].idxb];
+			newArr[swaps[i].idxb] = temp;
+			i++;
 
-      let temp = newArr[swaps[i].idxa];
-      newArr[swaps[i].idxa] = newArr[swaps[i].idxb];
-      newArr[swaps[i].idxb] = temp;
-      i++;
+			setState({ array: newArr, compared: comparedElements });
 
-      this.setState({ array: newArr, compared: comparedElements });
+			if (i === swaps.length) {
+				comparedElements[comparisons[i - 1].idxa] = false;
+				comparedElements[comparisons[i - 1].idxb] = false;
 
-      if (i === swaps.length) {
-        comparedElements[comparisons[i - 1].idxa] = false;
-        comparedElements[comparisons[i - 1].idxb] = false;
+				setState((prev) => ({
+					...prev,
+					compared: comparedElements,
+				}));
 
-        this.setState({ compared: comparedElements });
+				clearInterval(intervalID);
 
-        clearInterval(intervalID);
+				if (RESET_TIMEOUT !== "none") {
+					setTimeout(() => {
+						setState((prev) => ({
+							...prev,
+							array: resetArray(ARRAY_SIZE),
+						}));
+					}, RESET_TIMEOUT);
+				}
+			}
+		}, UPDATE_SPEED);
+	};
 
-        if (RESET_TIMEOUT !== 'none') {
-          setTimeout(() => {
-            this.setState({ array: resetArray(ARRAY_SIZE) });
-          }, RESET_TIMEOUT);
-        }
-      }
-    }, UPDATE_SPEED);
-  };
+	useEffect(() => {
+		setState((prev) => ({
+			...prev,
+			array: generateArray(ARRAY_SIZE),
+		}));
+	}, []);
 
-  render() {
-    return (
-      <>
-        <FLexContainer>
-          <ToolbarContainer>
-            <Button primary={true} clicked={this.generateArray}>
-              Generate Array
-            </Button>
-            <Button primary={false}>Bubble Sort</Button>
-            <Button primary={false}>Quick Sort</Button>
-            <Button primary={false}>Merge Sort</Button>
-          </ToolbarContainer>
-          <SortButtonContainer>
-            <Button primary={true} clicked={this.sortArray}>
-              Sort
-            </Button>
-          </SortButtonContainer>
-        </FLexContainer>
-        <MainContainer>
-          {this.state.array.map((el, idx) => (
-            <Bar height={el} isCompared={this.state.compared[idx]} key={idx} />
-          ))}
-        </MainContainer>
-      </>
-    );
-  }
-}
+	return (
+		<>
+			<FLexContainer>
+				<ToolbarContainer>
+					<Button primary={true} clicked={generateArray}>
+						Generate Array
+					</Button>
+				</ToolbarContainer>
+				<SortButtonContainer>
+					<Button primary={true} clicked={sortArray}>
+						Sort
+					</Button>
+				</SortButtonContainer>
+			</FLexContainer>
+			<MainContainer>
+				{state.array.map((el, idx) => (
+					<Bar height={el} isCompared={state.compared[idx]} key={idx} />
+				))}
+			</MainContainer>
+		</>
+	);
+};
 
 export default SortingVisualizer;
